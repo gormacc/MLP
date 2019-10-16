4c30b9023d29050aafc78a67f6852d714f26991a
Maciek <maciek.gorczyca@poczta.fm>
2019-10-16 22:38:32 +0200

    first try of xor

---
+++ NeuralNetwork.py
@@ -0,0 +1,88 @@
+import numpy as np
+
+class NeuralNetwork:
+
+    def __init__(self, inputNodes, hiddenNodes, outputNodes):
+        
+        self.inputNodes = inputNodes
+        self.hiddenNodes = hiddenNodes
+        self.outputNodes = outputNodes
+
+        self.firstWeights = self.initializeWeights(self.hiddenNodes, self.inputNodes)
+        self.secondWeights = self.initializeWeights(self.outputNodes, self.hiddenNodes)
+
+        self.firstBias = self.initializeWeights(self.hiddenNodes, 1)
+        self.secondBias = self.initializeWeights(self.outputNodes, 1)
+
+        self.setLearningRate()
+
+    def initializeWeights(self, rows, columns):
+        return np.matrix(np.random.rand(rows, columns) * 2 - 1)
+
+    def setLearningRate(self, learningRate = 0.2):
+        self.learningRate = learningRate
+
+    def predict(self, inArray):
+
+        sigmoid = np.vectorize(lambda x : 1 / (1 + np.exp(-x)))
+
+        inputLayer = np.matrix(inArray)
+        hiddenLayer = np.dot(self.firstWeights, inputLayer)
+        hiddenLayer = np.add(hiddenLayer, self.firstBias)
+        hiddenLayer = sigmoid(hiddenLayer)
+
+        outputLayer = np.dot(self.secondWeights, hiddenLayer)
+        outputLayer = np.add(outputLayer, self.secondBias)
+        outputLayer = sigmoid(outputLayer)
+
+        return outputLayer
+
+
+    def train(self, inArray, corrArray):
+
+        sigmoid = np.vectorize(lambda x : 1 / (1 + np.exp(-x)))
+        dsigmoid = np.vectorize(lambda x : x * (1 - x))
+
+        inputLayer = np.matrix(inArray)
+        inputLayer = np.transpose(inputLayer)
+        correctAnswers = np.matrix(corrArray)
+        correctAnswers = np.transpose(correctAnswers)
+
+        hiddenLayer = np.dot(self.firstWeights, inputLayer)
+        hiddenLayer = np.add(hiddenLayer, self.firstBias)
+        hiddenLayer = sigmoid(hiddenLayer)
+
+        outputLayer = np.dot(self.secondWeights, hiddenLayer)
+        outputLayer = np.add(outputLayer, self.secondBias)
+        outputLayer = sigmoid(outputLayer)
+
+        secondErrors = np.subtract(correctAnswers, outputLayer)
+        secondErrors = np.transpose(secondErrors)
+
+        secondGradient = dsigmoid(outputLayer)
+        secondGradient = np.dot(secondGradient, secondErrors)
+        secondGradient = np.multiply(secondGradient, self.learningRate)
+
+        transposedHiddenLayer = np.transpose(hiddenLayer)
+        deltaSecondWeights = np.dot(secondGradient, transposedHiddenLayer)
+
+        self.secondWeights = np.add(self.secondWeights, deltaSecondWeights)
+        self.secondBias = np.add(self.secondBias, secondGradient)
+
+        transposedSecondWeights = np.transpose(self.secondWeights)
+        firstErrors = np.dot(transposedSecondWeights, secondErrors)
+        firstErrors = np.transpose(firstErrors)
+
+        firstGradient = dsigmoid(hiddenLayer)
+        firstGradient = np.dot(firstGradient, firstErrors)
+        firstGradient = np.multiply(firstGradient, self.learningRate)
+
+        transposedInputLayer = inputLayer #np.transpose(inputLayer)
+        deltaFirstWeights = np.dot(firstGradient, transposedInputLayer)
+
+        self.firstWeights = np.add(self.firstWeights, deltaFirstWeights)
+        self.firstBias = np.add(self.firstBias, firstGradient)
+
+
+
+
---
+++ main.py
@@ -0,0 +1,18 @@
+import numpy as np
+import NeuralNetwork as nn
+
+
+valDictionary = { (0,0) : [0], (0,1) : [1] , (1,0) : [1], (1,1) : [0] }
+trueDictionary = {True : 1, False : 0}
+
+neuralNetwork = nn.NeuralNetwork(2, 2, 1)
+
+for _ in range(0, 10000):
+    firstBit = 0 + np.random.rand() < 0.5
+    secondBit = 0 + np.random.rand() < 0.5
+    neuralNetwork.train([trueDictionary[firstBit], trueDictionary[secondBit]], valDictionary[(firstBit, secondBit)] )
+
+print(neuralNetwork.predict([0,0]))
+print(neuralNetwork.predict([1,0]))
+print(neuralNetwork.predict([0,1]))
+print(neuralNetwork.predict([1,1]))
