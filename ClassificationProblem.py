ba801251174a8f213c2a73906516984c523dc895
Karol <Karolbienko1210@gmail.com>
2019-11-01 23:23:57 +0100

    Visualization

--- ClassificationProblem.py
+++ ClassificationProblem.py
@@ -6,11 +6,10 @@ import matplotlib.pyplot as plt
 
 #values to configure
 
-nodes = [2,7,2]
 seed = 1
 learningRate = 0.1
 useBiases = False
-trainLoops = 10
+trainLoops = 100
 
 midActivationFun = f.sigmoid
 endActivationFun = f.sigmoid
@@ -20,17 +19,17 @@ lossFun = f.crossEntropy
 
 #data paths
 
-trainDataPath = "C:/Users/macie/Desktop/studia/SieciNeuronowe/MLP/SN_projekt1/classification/data.simple.train.500.csv"
-testDataPath = "C:/Users/macie/Desktop/studia/SieciNeuronowe/MLP/SN_projekt1/classification/data.simple.test.500.csv"
+trainDataPath = "C:/Users/Karol/Desktop/ProjOB/SN/SN_projekt1/classification/data.simple.train.500.csv"
+testDataPath = "C:/Users/Karol/Desktop/ProjOB/SN/SN_projekt1/classification/data.simple.test.500.csv"
 
 #loading data
 fileHelper = fh.FileHelper()
 # data = fileHelper.LoadClassificationData(trainDataPath)
 # testData = fileHelper.LoadClassificationData(testDataPath)
-data = fileHelper.LoadClassificationData()
+trainData = fileHelper.LoadClassificationData()
 testData = fileHelper.LoadClassificationData()
 
-maxCls = max(data, key= lambda x: x.cls).cls
+maxCls = max(trainData, key= lambda x: x.cls).cls
 nodes = [2,7,maxCls]
 
 #initializing neural network
@@ -44,26 +43,55 @@ errors = []
 errSum = 0
 #training
 for _ in range(0, trainLoops):
-    for d in data:
+    for d in trainData:
         predicted = neuralNetwork.train(d.inputData(), d.correctResult(nodes[-1]))
         if d.cls != predicted.argmax() + 1:
             errSum = errSum + 1
         errors.append(errSum / (len(errors)+1))
 
 pCls = []
+trainCls = []
 
 for d in testData:
     p = neuralNetwork.predict(d.inputData())[-1]
     c = p.argmax() + 1
     pCls.append(c)
 
+for d in trainData:
+    p = neuralNetwork.predict(d.inputData())[-1]
+    c = p.argmax() + 1
+    trainCls.append(c)
+
 colors = {1:'red', 2:'green', 3:'blue', 4:'yellow'}
 fig, ax = plt.subplots()
 for i in range(len(testData)):
     ax.scatter(testData[i].x, testData[i].y,color=colors[pCls[i]])
+plt.title("Klasyfikacja na zbiorze testowym")
 
-plt.show()
+fig2, ax2 = plt.subplots()
+for i in range(len(trainData)):
+    ax2.scatter(trainData[i].x, trainData[i].y,color=colors[trainCls[i]])
+plt.title("Klasyfikacja na zbiorze treningowym")
 
+fig2, ax2 = plt.subplots()
+for i in range(len(testData)):
+    if (pCls[i] != testData[i].cls):
+        ax2.scatter(testData[i].x, testData[i].y,color='red')
+    else: 
+        ax2.scatter(testData[i].x, testData[i].y,color='green')
+plt.title("Błędy na zbiorze testowym")
+
+fig2, ax2 = plt.subplots()
+for i in range(len(trainData)):
+    if (trainCls[i] != trainData[i].cls):
+        ax2.scatter(trainData[i].x, trainData[i].y,color='red')
+    else: 
+        ax2.scatter(trainData[i].x, trainData[i].y,color='green')
+plt.title("Błędy na zbiorze treningowym")
+# plt.show()
+
+plt.figure()
 plt.plot(range(1, len(errors)+1), errors)
+plt.title("Errors over train times")
 
 plt.show()
--- RegressionProblem.py
+++ RegressionProblem.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 #values to configure
 
-nodes = [1,13,1]
+nodes = [1,5 , 13,1]
 seed = 1
 learningRate = 0.001
 useBiases = False
@@ -27,7 +27,7 @@ testDataPath = "C:/Users/macie/Desktop/studia/SieciNeuronowe/MLP/SN_projekt1/reg
 fileHelper = fh.FileHelper()
 # data = fileHelper.LoadRegressionData(trainDataPath)
 # testData = fileHelper.LoadRegressionData(testDataPath)
-data = fileHelper.LoadRegressionData()
+trainData = fileHelper.LoadRegressionData()
 testData = fileHelper.LoadRegressionData()
 
 #initializing neural network
@@ -41,7 +41,7 @@ errors = []
 
 #training
 for _ in range(0, trainLoops):
-    for d in data:
+    for d in trainData:
         predictedValue = neuralNetwork.train(d.inputData(), d.correctResult())
         errors.append(np.abs( d.y - predictedValue.item(0) ) )
 
@@ -49,20 +49,34 @@ for _ in range(0, trainLoops):
 
 x = []
 y = []
+xtrain = []
+ytrain = []
 yp = []
+yptrain = []
 
 for d in sorted(testData, key = lambda td: td.x):
     x.append(d.x)
     y.append(d.y)
     yp.append(neuralNetwork.predict(d.inputData())[-1].item(0))
 
+for d in sorted(trainData, key = lambda td: td.x):
+    xtrain.append(d.x)
+    ytrain.append(d.y)
+    yptrain.append(neuralNetwork.predict(d.inputData())[-1].item(0))
+
 plt.figure(1)
 plt.plot(x, yp, 'ob', label = 'Predicted data')
 plt.plot(x, y, 'r', label = 'Correct data')
-plt.title('Prediction')
+plt.title('Prediction on test set')
 plt.legend()
 
 plt.figure(2)
+plt.plot(xtrain, yptrain, 'ob', label = 'Predicted data')
+plt.plot(xtrain, ytrain, 'or', label = 'Correct data')
+plt.title('Prediction on train set')
+plt.legend()
+
+plt.figure(3)
 plt.plot(range(1, len(errors)+1), errors)
 plt.title('Errors over train times')
 plt.show()
